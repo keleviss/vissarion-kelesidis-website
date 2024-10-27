a67bfb1da95657772ec426b78886afb4ad1f0780
keleviss <kelesidisvissarion@gmail.com>
2024-10-27 17:40:25 +0200

    Refactor navbar padding and add off-canvas menu for smaller screens

--- script.js
+++ script.js
@@ -75,3 +75,20 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
 });
+
+// Off Canvas Menu
+const mobileNavMenu =  document.getElementById('mobile-nav-menu');
+const offCanvasMenu = document.getElementById('offcanvas-menu');
+const offCanvasCloseIcon = document.getElementById('offcanvas-close-icon'); 
+
+mobileNavMenu.addEventListener('click', event => {
+  // offCanvasMenu.style.visibility = 'visible';
+  offCanvasMenu.style.transform = 'translateX(0)';
+  document.body.style.overflowY = 'hidden';
+  offCanvasMenu.style.position = 'fixed';
+})
+
+offCanvasCloseIcon.addEventListener('click', event => {
+  offCanvasMenu.style.transform = 'translateY(-100%)';
+  document.body.style.overflowY = 'unset';
+})
